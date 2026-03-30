ddfa10e25d077c8827afbe1a785f0cdd664128d8
Alfonso Adiego Luque <alfonsoaldev@gmail.com>
2026-03-30 20:37:27 +0200

    Adding icons and projects

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,7 +1,13 @@
 import logoAlfonso from '@img/logo.png';
+import GitHubIcon from '@icon/github_dark.svg';
+import TailWindIcon from '@icon/tailwindcss.svg';
 
 export const Images = {
   logos: {
     logoAlfonso: logoAlfonso,
   },
+  icons: {
+    GitHubIcon: GitHubIcon,
+    TailWindIcon: TailWindIcon,
+  }
 };
